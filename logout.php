c5595d3d7742cb47072385b7c61fdd094086f27f
Talita Oliveira <talitagiovanna2011@gmail.com>
2024-12-01 10:25:04 -0300

    Adjusting things

--- logout.php
+++ logout.php
@@ -8,6 +8,11 @@
   // Destrói a sessão
   session_destroy();
 
+  // Limpa o cookie de sessão (para garantir que a sessão não seja mantida)
+  if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');  // Expira o cookie de sessão
+  }
+
   // Redireciona o usuário para a página inicial ou de login
   header("Location: index.html");  // Ou para login.php, se for o caso
   exit();
